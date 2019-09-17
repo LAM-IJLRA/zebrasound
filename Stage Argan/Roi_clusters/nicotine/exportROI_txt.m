5cae150f3b22c3ffbc7fcb8503b2752509fda049
ArganVerrier <51755010+ArganVerrier@users.noreply.github.com>
2019-09-17 16:37:22 +0200

    Create exportROI_txt.m

---
+++ Stage Argan/Roi_clusters/nicotine/exportROI_txt.m
@@ -0,0 +1,22 @@
+clear all, close all
+
+% Convertit les roi en coll, format ['nTop', 'nLeft', 'nBottom', 'nRight']
+
+data='Roiset.zip';
+file='roitext_nicotine.txt';
+
+roi=ReadImageJROI('RoiSet.zip');
+
+pos=[];
+index=[];
+delim=[];
+N=length(roi);
+
+for ind=1:N 
+    index=cat(1, index, strjoin(strcat(string(ind), ','))); 
+    pos=cat(1,pos, roi{ind}.vnRectBounds);
+    delim=cat(1, delim, ";");
+end
+
+tab=table(index, pos, delim);
+writetable(tab, file, 'delimiter', ' ', 'WriteVariableNames', 0)
